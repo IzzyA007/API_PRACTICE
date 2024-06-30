7c5ae2acd33ebd731e1273d11768ecd5ed8dbaae
Israel Akpati <izzyakpati@gmail.com>
2024-06-29 19:57:06 -0500

    Buttons in tkinter

--- DEMO_PRACTICE#1.py
+++ DEMO_PRACTICE#1.py
@@ -1,9 +1,12 @@
 from tkinter import*
 
 root = Tk()
+def myClick():
+    myLabel1 = Label(root, text= "My botton works")
+    myLabel1.pack()
 
-myButton = Button(root, text= "Click Me!", state=DISABLED, padx=500)
-#When you put disabled this would disable the button
+myButton = Button(root, text= "Click Me!", command=myClick)
+# Command= makes the button run a function
 # padx is like the padding of the botton x is left and right and y is up and down
 
 myButton.pack() #This just packs it into the window
