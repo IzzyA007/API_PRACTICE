3d588236c36dbe5eb22a2df4b22e2c579dcdd994
Israel Akpati <izzyakpati@gmail.com>
2024-06-29 23:02:10 -0500

    Inserting words in the text field and also arranging the buttons

--- DEMO_PRACTICE#1.py
+++ DEMO_PRACTICE#1.py
@@ -1,11 +1,17 @@
 from tkinter import*
 
 root = Tk()
+
+e = Entry(root, )
+e.pack()
+e.get()
+e.insert(0, "Enter your name")
+
 def myClick():
-    myLabel1 = Label(root, text= "My botton works")
+    myLabel1 = Label(root,text= "Yooooo wassup " + e.get(), )
     myLabel1.pack()
 
-myButton = Button(root, text= "Click Me!", command=myClick)
+myButton = Button(root, text= "Enter Your Name", command=myClick,)
 # Command= makes the button run a function
 # padx is like the padding of the botton x is left and right and y is up and down
 
