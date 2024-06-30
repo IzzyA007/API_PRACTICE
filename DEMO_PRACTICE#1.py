40d1bc33f436f012b7ea731ad858a843ab003a24
Israel Akpati <izzyakpati@gmail.com>
2024-06-29 19:12:09 -0500

    Tkinter Grid System Part/Section

--- DEMO_PRACTICE#1.py
+++ DEMO_PRACTICE#1.py
@@ -2,9 +2,13 @@ from tkinter import*
 
 root = Tk()
 # Creating a Lebel Widget
-myLabel = Label(root, text= "My name is Izzy")
+myLabel1 = Label(root, text= "My name is Izzy").grid(row=0, column=0)
+myLabel2 = Label(root, text= "I like Ferrari").grid(row=1, column=0)
+myLabel3 = Label(root, text= " NBA")
 
 # Shoving it onto the screen
-myLabel.pack() #It basically fits whatever in your texts prompt on the screen
+# myLabel1.grid(row=1, column=5)
+# myLabel2.grid(row=1, column=5)
+# myLabel3.grid(row=1, column=5)
 
 root.mainloop() #This runs the application and keeps it going
