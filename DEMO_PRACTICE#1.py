fb15b72f0831a0fea1fa1142bf715e3edded55a3
Israel Akpati <izzyakpati@gmail.com>
2024-06-29 19:26:53 -0500

    Disable and padding updates

--- DEMO_PRACTICE#1.py
+++ DEMO_PRACTICE#1.py
@@ -1,14 +1,12 @@
 from tkinter import*
 
 root = Tk()
-# Creating a Lebel Widget
-myLabel1 = Label(root, text= "My name is Izzy").grid(row=0, column=0)
-myLabel2 = Label(root, text= "I like Ferrari").grid(row=1, column=0)
-myLabel3 = Label(root, text= " NBA")
 
-# Shoving it onto the screen
-# myLabel1.grid(row=1, column=5)
-# myLabel2.grid(row=1, column=5)
-# myLabel3.grid(row=1, column=5)
+myButton = Button(root, text= "Click Me!", state=DISABLED, padx=500)
+#When you put disabled this would disable the button
+# padx is like the padding of the botton x is left and right and y is up and down
+
+myButton.pack() #This just packs it into the window
+
 
 root.mainloop() #This runs the application and keeps it going
