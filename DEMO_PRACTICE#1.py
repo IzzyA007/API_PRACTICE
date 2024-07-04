27db27b31c601c4d9338dd6c1ac786cd44784e94
Israel Akpati <izzyakpati@gmail.com>
2024-07-04 14:34:28 -0500

    Part 3 of the Tkinter Python Calculator The calculator is completed and overall looks good and works well can change somethings around but thats to be extra overall easy and nice to get the hang of tkinter to work on other projects

--- DEMO_PRACTICE#1.py
+++ DEMO_PRACTICE#1.py
@@ -3,7 +3,7 @@ from tkinter import*
 root = Tk()
 root.title("Izzy Calculator")
 
-e = Entry(root, width=35, borderwidth=5)
+e = Entry(root, width=90, borderwidth=5)
 e.grid(column=0, row=0, columnspan=3, padx=10, pady=10)
 
 #e.insert(0, "Enter your name")
@@ -19,13 +19,50 @@ def button_clear():
 def button_add():
     first_number = e.get()
     global f_num
+    global math
+    math = "addtion"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+def button_subtraction():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "subtraction"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+def button_multiplication():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "multiplication"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+def button_Divide():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "division"
     f_num = int(first_number)
     e.delete(0, END)
 
 def button_equal():
     second_number = e.get() # This is going to pull whatever is in that textbox
     e.delete(0, END)
-    e.insert(0,f_num + int(second_number))
+    if math == "addtion":
+        e.insert(0,f_num + int(second_number))
+
+    if math == "subtraction":
+        e.insert(0,f_num - int(second_number))
+
+    if math == "multiplication":
+        e.insert(0,f_num * int(second_number))
+
+    if math == "division":
+        e.insert(0,f_num / int(second_number))
+
 
 
 
@@ -33,22 +70,22 @@ def button_equal():
 
 # Defined the buttons to use
 
-button_1 = Button(root, text="1", padx=80, pady=20, command=lambda :button_click(1) ) #To run the number for each function we need a lambada
-button_2 = Button(root, text="2", padx=40, pady=20, command=lambda :button_click(2) )
-button_3 = Button(root, text="3", padx=40, pady=20, command=lambda :button_click(3) )
-button_4 = Button(root, text="4", padx=80, pady=20, command=lambda :button_click(4) )
-button_5 = Button(root, text="5", padx=40, pady=20, command=lambda :button_click(5) )
-button_6 = Button(root, text="6", padx=40, pady=20, command=lambda :button_click(6) )
-button_7 = Button(root, text="7", padx=80, pady=20, command=lambda :button_click(7) )
-button_8 = Button(root, text="8", padx=40, pady=20, command=lambda :button_click(8) )
-button_9 = Button(root, text="9", padx=40, pady=20, command=lambda :button_click(9) )
-button_0 = Button(root, text="0", padx=80, pady=20, command=lambda :button_click(0) )
+button_1 = Button(root, text="1", padx=80, pady=30, command=lambda :button_click(1) ) #To run the number for each function we need a lambada
+button_2 = Button(root, text="2", padx=80, pady=30, command=lambda :button_click(2) )
+button_3 = Button(root, text="3", padx=80, pady=30, command=lambda :button_click(3) )
+button_4 = Button(root, text="4", padx=80, pady=30, command=lambda :button_click(4) )
+button_5 = Button(root, text="5", padx=80, pady=30, command=lambda :button_click(5) )
+button_6 = Button(root, text="6", padx=80, pady=30, command=lambda :button_click(6) )
+button_7 = Button(root, text="7", padx=80, pady=30, command=lambda :button_click(7) )
+button_8 = Button(root, text="8", padx=80, pady=30, command=lambda :button_click(8) )
+button_9 = Button(root, text="9", padx=80, pady=30, command=lambda :button_click(9) )
+button_0 = Button(root, text="0", padx=80, pady=30, command=lambda :button_click(0) )
 enter_button = Button(root, text="=", padx=80,pady=30,command=button_equal)
-add_button = Button(root, text="+", padx=40,pady=20,command=button_add)
-subtract_button = Button(root, text="-", padx=40,pady=20,command=lambda :button_click())
-divide_button = Button(root, text="/", padx=40,pady=20,command=lambda :button_click())
+add_button = Button(root, text="+", padx=80,pady=30,command=button_add)
+subtract_button = Button(root, text="-", padx=80,pady=30,command=button_subtraction)
+divide_button = Button(root, text="/", padx=80,pady=30,command=button_Divide)
 clear_button = Button(root, text="C", padx=80,pady=30,command=button_clear)
-multiply_button = Button(root,text="*",padx=80,pady=30,command=lambda :button_click())
+multiply_button = Button(root,text="*",padx=80,pady=30,command=button_multiplication)
 
 #Put the buttons on the screen
 
@@ -70,7 +107,7 @@ enter_button.grid(row=5, column=0)
 add_button.grid(row=4, column=1)
 subtract_button.grid(row=4, column=2)
 multiply_button.grid(row=5, column=2)
-divide_button.grid(row=5, column=2)
+divide_button.grid(row=5, column=4)
 clear_button.grid(row=5, column=1)
 
 
